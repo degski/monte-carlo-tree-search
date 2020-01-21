a88e0bfe127438d82c12bf6cf8a98bed540ce9c2
degski <degski@gmail.com>
2020-01-21 07:42:04 -0600

    added hashing, constexpr lookup-tables

--- games/connect_four.h
+++ games/connect_four.h
@@ -8,7 +8,7 @@
 
 #include "multi_array.hpp"
 
-template<int NumRows = 6, int NumCols = 7>
+template<std::size_t NumRows = 6, std::size_t NumCols = 7>
 class ConnectFourState {
     public:
     using Move                = int;
@@ -25,6 +25,13 @@ class ConnectFourState {
         board.resize ( NumRows, std::vector<char> ( NumCols, player_markers[ 0 ] ) );
     }
 
+    // Returns the hash of the board xor'ed with the player's hash.
+    // For outside consumption.
+    ZobristHash zobrist ( ) const noexcept {
+        // Order of hashes (it is inverted) is not relevant, as long it's the same every time.
+        return m_zobrist_hash ^ m_zobrist_player_keys[ player_to_move ];
+    }
+
     void do_move ( Move move ) {
         attest ( 0 <= move && move < NumCols );
         attest ( board[ 0 ][ move ] == player_markers[ 0 ] );
@@ -32,6 +39,24 @@ class ConnectFourState {
         int row = NumRows - 1;
         while ( board[ row ][ move ] != player_markers[ 0 ] )
             row--;
+
+        board[ row ][ move ] = player_markers[ player_to_move ];
+        last_col             = move;
+        last_row             = row;
+
+        player_to_move = 3 - player_to_move;
+    }
+
+    void do_hash_move ( Move move ) {
+        attest ( 0 <= move && move < NumCols );
+        attest ( board[ 0 ][ move ] == player_markers[ 0 ] );
+
+        int row = NumRows - 1;
+        while ( board[ row ][ move ] != player_markers[ 0 ] )
+            row--;
+
+        m_zobrist_hash ^=
+            m_zobrist_keys.at ( player_to_move, row, move ); // player_to_move is here the player who is makeing a move.
         board[ row ][ move ] = player_markers[ player_to_move ];
         last_col             = move;
         last_row             = row;
@@ -166,16 +191,47 @@ class ConnectFourState {
 
     int player_to_move;
 
+    static constexpr ZobristHashKeys m_zobrist_keys = {
+
+        0xa1a656cb9731c5d5ull, 0xc3dce6ad6465ea7aull, 0x9e2556e2bbec18d3ull, 0x900670630f4f76afull, 0xda8071005889fa3cull,
+        0xd1efb50aec8b61a9ull, 0x73203d10cf4db8b8ull, 0x6ab7fd70679d877full, 0x3a56cdae74f9d816ull, 0xb3b48dc62bacaf9bull,
+        0x27760b12660e6c3bull, 0xd9ac7fb482854702ull, 0xd35e698b064e4f93ull, 0x7b379503f68242bdull, 0xdad6afcb4409d282ull,
+        0xf04b592c8e1183feull, 0x6dbb4f77e63f5267ull, 0x970b0ae4e9e7d347ull, 0xd19027f157c2845aull, 0x82a53746e2d25fa5ull,
+        0xe2097dbb17c142f7ull, 0x5eba98d936a14c91ull, 0x963286f60ab69777ull, 0x96e9eb899e5e615bull, 0xecd8957747d0bef8ull,
+        0x961b3fb52b112218ull, 0x44c776ac7af4cc2dull, 0xfa2708e399719ac4ull, 0xe34b58c2f6acac45ull, 0x7f6d2cb0416a63caull,
+        0x287ecf88477a3e7dull, 0xe57d268150b95703ull, 0xf9cc76357617493cull, 0xe956f77acaa2f112ull, 0x9a9441286a0a70e7ull,
+        0x5b5a62ba1d8dfd33ull, 0xb3d1b947205bf8f4ull, 0x4aabdee7fb6aa20bull, 0xa810d257d77576afull, 0x6a1789922b7af41aull,
+        0x315833a0f0b5ceebull, 0x481a32e97fbd47d8ull, 0x11e80a41d2022fdcull, 0xfab59400ba6c780cull, 0xfce9f47e1dc3037dull,
+        0xf5f404421f6c78b2ull, 0x274ef7151bd8503eull, 0x1d5268cdadd43ad3ull, 0x59ed9dc04b81a0c1ull, 0x3c10ea92d1a6d79dull,
+        0x595d9292d07ee51dull, 0x1a62a32bb174ee71ull, 0x417fd9b9b0bc7a47ull, 0x3e266eca431347d6ull, 0x74a093aeceb1fd60ull,
+        0x7720a5e78ae8d571ull, 0x9645ae72f6f57362ull, 0xcc7279ab05731ef7ull, 0xf5a0574bc2385c6full, 0xb254ccf017ebc43bull,
+        0x34184cd5945aff3eull, 0x4c5ede78a68fd1a5ull, 0x49adf513d838ce5dull, 0x44940842e2c75c16ull, 0x7aacd877d0831e19ull,
+        0x9d8d5e4f7c511acdull, 0xac2f78583e0e9692ull, 0x03e2da677110440cull, 0x07d2a6b527f4ef05ull, 0x91a680f12222cf16ull,
+        0x08617f45641626d0ull, 0xb2df85147e2a11cbull, 0x6bf333747f7f10a4ull, 0xc6f2a33e3a94b2c1ull, 0xf5358b1cb75e528full,
+        0x904af33725c150b5ull, 0xd75d6d3f202f964bull, 0x8d58eeece3979331ull, 0xb58f905351a0d8f1ull, 0x38ad67581ffcbdfbull,
+        0xcd5f48e9ac464398ull, 0xfcc2df3237564c0cull, 0x1ea8202ddf77efdeull, 0x000617fafba044adull
+    };
+    static constexpr ZobristHash m_zobrist_player_keys[ 3 ] = {
+
+        0x41fec34015a1bef2ull, 0x8b80677c9c144514ull, 0xf6242292160d5bb7ull
+    };
+    static constexpr int max_no_moves = NumCols;
+
     private:
+    ZobristHash m_zobrist_hash = m_zobrist_player_keys[ 0 ]; // Hash of the current m_board, irrespective of who played last.
+
     vector<vector<char>> board;
     int last_col, last_row;
 };
 
-template<int NumRow, int NumCols>
+// Spare hash keys...
+// 0xe028283c7b3c8bc3ull, 0x0fce58188743146dull, 0x5c0d56eb69eac805ull
+
+template<std::size_t NumRow, std::size_t NumCols>
 ostream & operator<< ( ostream & out, const ConnectFourState<NumRow, NumCols> & state ) {
     state.print ( out );
     return out;
 }
 
-template<int NumRow, int NumCols>
+template<std::size_t NumRow, std::size_t NumCols>
 const char ConnectFourState<NumRow, NumCols>::player_markers[ 3 ] = { '.', 'X', 'O' };
--- games/multi_array.hpp
+++ games/multi_array.hpp
@@ -32,307 +32,257 @@
 
 namespace ma {
 
-template < typename T, intptr_t I, intptr_t BaseI = 0, typename = std::enable_if_t < std::is_default_constructible<T>::value, T > >
+template<typename T, intptr_t I, intptr_t BaseI = 0, typename = std::enable_if_t<std::is_default_constructible<T>::value, T>>
 class Vector {
 
-	T m_data [ I ];
+    T m_data[ I ];
 
-public:
+    public:
+    Vector ( ) : m_data{ T ( ) } {}
+    Vector ( const Vector & v_ ) { memcpy ( m_data, v_.m_data, I * sizeof ( T ) ); }
+    Vector ( Vector && v_ ) { memcpy ( m_data, v_.m_data, I * sizeof ( T ) ); }
+    template<typename... Args>
+    constexpr Vector ( Args... a_ ) : m_data{ a_... } {}
 
-	Vector ( ) : m_data { T ( ) } { }
-	Vector ( const Vector & v_ ) { memcpy ( m_data, v_.m_data, I * sizeof ( T ) ); }
-	Vector ( Vector && v_ ) { memcpy ( m_data, v_.m_data, I * sizeof ( T ) ); }
-	template < typename ... Args >
-	Vector ( Args ... a_ ) : m_data { a_ ... } { }
+    typedef std::array<intptr_t, 1> extents_type;
 
-	typedef std::array < intptr_t, 1 > extents_type;
+    T & at ( const intptr_t i_ ) {
 
-	T &at ( const intptr_t i_ ) {
+        assert ( i_ >= BaseI && i_ < I + BaseI );
 
-		assert ( i_ >= BaseI && i_ < I + BaseI );
+        return ( m_data - BaseI )[ i_ ];
+    }
 
-		return ( m_data - BaseI ) [ i_ ];
-	}
+    T at ( const intptr_t i_ ) const {
 
-	T at ( const intptr_t i_ ) const {
+        assert ( i_ >= BaseI && i_ < I + BaseI );
 
-		assert ( i_ >= BaseI && i_ < I + BaseI );
+        return ( m_data - BaseI )[ i_ ];
+    }
 
-		return ( m_data - BaseI ) [ i_ ];
-	}
+    T & at_r ( const intptr_t i_ ) {
 
-	T &at_r ( const intptr_t i_ ) {
+        assert ( i_ >= BaseI && i_ < I + BaseI );
 
-		assert ( i_ >= BaseI && i_ < I + BaseI );
+        return ( m_data + I - 1 + BaseI )[ -i_ ];
+    }
 
-		return ( m_data + I - 1 + BaseI ) [ -i_ ];
-	}
+    T at_r ( const intptr_t i_ ) const {
 
-	T at_r ( const intptr_t i_ ) const {
+        assert ( i_ >= BaseI && i_ < I + BaseI );
 
-		assert ( i_ >= BaseI && i_ < I + BaseI );
+        return ( m_data + I - 1 + BaseI )[ -i_ ];
+    }
 
-		return ( m_data + I - 1 + BaseI ) [ -i_ ];
-	}
+    T * data ( ) { return m_data; }
 
-	T *data ( ) {
+    const T * data ( ) const { return m_data; }
 
-		return m_data;
-	}
+    std::size_t size ( ) const { return I; }
 
-	const T *data ( ) const {
-
-		return m_data;
-	}
-
-	std::size_t size ( ) const {
-
-		return I;
-	}
-
-	extents_type extents ( ) const {
-
-		return extents_type { I };
-	}
+    extents_type extents ( ) const { return extents_type{ I }; }
 };
 
-
-template<typename T, intptr_t I, intptr_t J, intptr_t BaseI = 0, intptr_t BaseJ = 0, typename = std::enable_if_t < std::is_default_constructible<T>::value, T > >
+template<typename T, intptr_t I, intptr_t J, intptr_t BaseI = 0, intptr_t BaseJ = 0,
+         typename = std::enable_if_t<std::is_default_constructible<T>::value, T>>
 class Matrix {
 
-	T m_data [ I * J ];
+    T m_data[ I * J ];
 
-public:
+    public:
+    Matrix ( ) : m_data{ T ( ) } {}
+    Matrix ( const Matrix & m_ ) { memcpy ( m_data, m_.m_data, I * J * sizeof ( T ) ); }
+    Matrix ( Matrix && m_ ) { memcpy ( m_data, m_.m_data, I * J * sizeof ( T ) ); }
+    template<typename... Args>
+    constexpr Matrix ( Args... a_ ) : m_data{ a_... } {}
 
-	Matrix ( ) : m_data { T ( ) } { }
-	Matrix ( const Matrix & m_ ) { memcpy ( m_data, m_.m_data, I * J * sizeof ( T ) ); }
-	Matrix ( Matrix && m_ ) { memcpy ( m_data, m_.m_data, I * J * sizeof ( T ) ); }
-	template < typename ... Args >
-	Matrix ( Args ... a_ ) : m_data { a_ ... } { }
+    typedef std::array<intptr_t, 2> extents_type;
 
-	typedef std::array < intptr_t, 2 > extents_type;
+    T const & ref ( const intptr_t i_, const intptr_t j_ ) const {
 
-	T const &ref ( const intptr_t i_, const intptr_t j_ ) const {
+        assert ( i_ >= BaseI );
+        assert ( i_ < I + BaseI );
+        assert ( j_ >= BaseJ );
+        assert ( j_ < J + BaseJ );
 
-		assert ( i_ >= BaseI );
-		assert ( i_ < I + BaseI );
-		assert ( j_ >= BaseJ );
-		assert ( j_ < J + BaseJ );
+        return ( m_data - BaseJ - BaseI * J )[ j_ + i_ * J ];
+    }
 
-		return ( m_data - BaseJ - BaseI * J ) [ j_ + i_ * J ];
-	}
+    T & at ( const intptr_t i_, const intptr_t j_ ) {
 
-	T &at ( const intptr_t i_, const intptr_t j_ ) {
+        assert ( i_ >= BaseI );
+        assert ( i_ < I + BaseI );
+        assert ( j_ >= BaseJ );
+        assert ( j_ < J + BaseJ );
 
-		assert ( i_ >= BaseI );
-		assert ( i_ < I + BaseI );
-		assert ( j_ >= BaseJ );
-		assert ( j_ < J + BaseJ );
+        return ( m_data - BaseJ - BaseI * J )[ j_ + i_ * J ];
+    }
 
-		return ( m_data - BaseJ - BaseI * J ) [ j_ + i_ * J ];
-	}
+    T at ( const intptr_t i_, const intptr_t j_ ) const {
 
-	T at ( const intptr_t i_, const intptr_t j_ ) const {
+        assert ( i_ >= BaseI );
+        assert ( i_ < I + BaseI );
+        assert ( j_ >= BaseJ );
+        assert ( j_ < J + BaseJ );
 
-		assert ( i_ >= BaseI );
-		assert ( i_ < I + BaseI );
-		assert ( j_ >= BaseJ );
-		assert ( j_ < J + BaseJ );
+        return ( m_data - BaseJ - BaseI * J )[ j_ + i_ * J ];
+    }
 
-		return ( m_data - BaseJ - BaseI * J ) [ j_ + i_ * J ];
-	}
+    T const & ref_r ( const intptr_t i_, const intptr_t j_ ) {
 
-	T const &ref_r ( const intptr_t i_, const intptr_t j_ ) {
+        // Mirror the matrix coordinates...
 
-		// Mirror the matrix coordinates...
+        assert ( i_ >= BaseI );
+        assert ( i_ < I + BaseI );
+        assert ( j_ >= BaseJ );
+        assert ( j_ < J + BaseJ );
 
-		assert ( i_ >= BaseI );
-		assert ( i_ < I + BaseI );
-		assert ( j_ >= BaseJ );
-		assert ( j_ < J + BaseJ );
+        return ( m_data + I * J - 1 + BaseJ + BaseI * J )[ -j_ - i_ * J ];
+    }
 
-		return ( m_data + I * J - 1 + BaseJ + BaseI * J ) [ -j_ - i_ * J ];
-	}
+    T & at_r ( const intptr_t i_, const intptr_t j_ ) {
 
-	T &at_r ( const intptr_t i_, const intptr_t j_ ) {
+        // Mirror the matrix coordinates...
 
-		// Mirror the matrix coordinates...
+        assert ( i_ >= BaseI );
+        assert ( i_ < I + BaseI );
+        assert ( j_ >= BaseJ );
+        assert ( j_ < J + BaseJ );
 
-		assert ( i_ >= BaseI );
-		assert ( i_ < I + BaseI );
-		assert ( j_ >= BaseJ );
-		assert ( j_ < J + BaseJ );
+        return ( m_data + I * J - 1 + BaseJ + BaseI * J )[ -j_ - i_ * J ];
+    }
 
-		return ( m_data + I * J - 1 + BaseJ + BaseI * J ) [ -j_ - i_ * J ];
-	}
+    T at_r ( const intptr_t i_, const intptr_t j_ ) const {
 
-	T at_r ( const intptr_t i_, const intptr_t j_ ) const {
+        // Mirror the matrix coordinates...
 
-		// Mirror the matrix coordinates...
+        assert ( i_ >= BaseI );
+        assert ( i_ < I + BaseI );
+        assert ( j_ >= BaseJ );
+        assert ( j_ < J + BaseJ );
 
-		assert ( i_ >= BaseI );
-		assert ( i_ < I + BaseI );
-		assert ( j_ >= BaseJ );
-		assert ( j_ < J + BaseJ );
+        return ( m_data + I * J - 1 + BaseJ + BaseI * J )[ -j_ - i_ * J ];
+    }
 
-		return ( m_data + I * J - 1 + BaseJ + BaseI * J ) [ -j_ - i_ * J ];
-	}
+    T * data ( ) { return m_data; }
 
-	T *data ( ) {
+    const T * data ( ) const { return m_data; }
 
-		return m_data;
-	}
+    std::size_t size ( ) const { return I * J; }
 
-	const T *data ( ) const {
-
-		return m_data;
-	}
-
-	std::size_t size ( ) const {
-
-		return I * J;
-	}
-
-	extents_type extents ( ) const {
-
-		return extents_type { I, J };
-	}
+    extents_type extents ( ) const { return extents_type{ I, J }; }
 };
 
-template<typename T, intptr_t I, intptr_t J, intptr_t BaseI = 0, intptr_t BaseJ = 0, typename = std::enable_if_t < std::is_default_constructible<T>::value, T > >
-using MatrixRM = Matrix < T, I, J, BaseI, BaseJ >;
+template<typename T, intptr_t I, intptr_t J, intptr_t BaseI = 0, intptr_t BaseJ = 0,
+         typename = std::enable_if_t<std::is_default_constructible<T>::value, T>>
+using MatrixRM = Matrix<T, I, J, BaseI, BaseJ>;
 
-template<typename T, intptr_t J, intptr_t I, intptr_t BaseJ = 0, intptr_t BaseI = 0, typename = std::enable_if_t < std::is_default_constructible<T>::value, T > >
-using MatrixCM = Matrix < T, J, I, BaseJ, BaseI >;
+template<typename T, intptr_t J, intptr_t I, intptr_t BaseJ = 0, intptr_t BaseI = 0,
+         typename = std::enable_if_t<std::is_default_constructible<T>::value, T>>
+using MatrixCM = Matrix<T, J, I, BaseJ, BaseI>;
 
-
-template < typename T, intptr_t I, intptr_t J, intptr_t K, intptr_t BaseI = 0, intptr_t BaseJ = 0, intptr_t BaseK = 0, typename = std::enable_if_t < std::is_default_constructible<T>::value, T > >
+template<typename T, intptr_t I, intptr_t J, intptr_t K, intptr_t BaseI = 0, intptr_t BaseJ = 0, intptr_t BaseK = 0,
+         typename = std::enable_if_t<std::is_default_constructible<T>::value, T>>
 class Cube {
 
-	T m_data [ I * J * K ];
+    T m_data[ I * J * K ];
 
-public:
+    public:
+    Cube ( ) : m_data{ T ( ) } {}
+    Cube ( const Cube & c_ ) { memcpy ( m_data, c_.m_data, I * J * K * sizeof ( T ) ); }
+    Cube ( Cube && c_ ) { memcpy ( m_data, c_.m_data, I * J * K * sizeof ( T ) ); }
+    template<typename... Args>
+    constexpr Cube ( Args... a_ ) : m_data{ a_... } {}
 
-	Cube ( ) : m_data { T ( ) } { }
-	Cube ( const Cube & c_ ) { memcpy ( m_data, c_.m_data, I * J * K * sizeof ( T ) ); }
-	Cube ( Cube && c_ ) { memcpy ( m_data, c_.m_data, I * J * K * sizeof ( T ) ); }
-	template < typename ... Args >
-	Cube ( Args ... a_ ) : m_data { a_ ... } { }
+    typedef std::array<intptr_t, 3> extents_type;
 
-	typedef std::array < intptr_t, 3 > extents_type;
+    T & at ( const intptr_t i_, const intptr_t j_, const intptr_t k_ ) {
 
-	T &at ( const intptr_t i_, const intptr_t j_, const intptr_t k_ ) {
+        assert ( i_ >= BaseI && i_ < I + BaseI );
+        assert ( j_ >= BaseJ && j_ < J + BaseJ );
+        assert ( k_ >= BaseK && k_ < K + BaseK );
 
-		assert ( i_ >= BaseI && i_ < I + BaseI );
-		assert ( j_ >= BaseJ && j_ < J + BaseJ );
-		assert ( k_ >= BaseK && k_ < K + BaseK );
+        return ( m_data + K * ( -BaseJ - BaseI * J ) - BaseK )[ K * ( j_ + i_ * J ) + k_ ];
+    }
 
-		return ( m_data + K * ( -BaseJ - BaseI * J ) - BaseK ) [ K * ( j_ + i_ * J ) + k_ ];
-	}
+    T at ( const intptr_t i_, const intptr_t j_, const intptr_t k_ ) const {
 
-	T at ( const intptr_t i_, const intptr_t j_, const intptr_t k_ ) const {
+        assert ( i_ >= BaseI && i_ < I + BaseI );
+        assert ( j_ >= BaseJ && j_ < J + BaseJ );
+        assert ( k_ >= BaseK && k_ < K + BaseK );
 
-		assert ( i_ >= BaseI && i_ < I + BaseI );
-		assert ( j_ >= BaseJ && j_ < J + BaseJ );
-		assert ( k_ >= BaseK && k_ < K + BaseK );
+        return ( m_data + K * ( -BaseJ - BaseI * J ) - BaseK )[ K * ( j_ + i_ * J ) + k_ ];
+    }
 
-		return ( m_data + K * ( -BaseJ - BaseI * J ) - BaseK ) [ K * ( j_ + i_ * J ) + k_ ];
-	}
+    T & at_r ( const intptr_t i_, const intptr_t j_, const intptr_t k_ ) {
 
-	T &at_r ( const intptr_t i_, const intptr_t j_, const intptr_t k_ ) {
+        assert ( i_ >= BaseI && i_ < I + BaseI );
+        assert ( j_ >= BaseJ && j_ < J + BaseJ );
+        assert ( k_ >= BaseK && k_ < K + BaseK );
 
-		assert ( i_ >= BaseI && i_ < I + BaseI );
-		assert ( j_ >= BaseJ && j_ < J + BaseJ );
-		assert ( k_ >= BaseK && k_ < K + BaseK );
+        return ( m_data + I * J * K - 1 + BaseJ * K + BaseI * J * K + BaseK )[ K * ( -j_ - i_ * J ) - k_ ];
+    }
 
-		return ( m_data + I * J * K - 1 + BaseJ * K + BaseI * J * K + BaseK ) [ K * ( -j_ - i_ * J ) - k_ ];
-	}
+    T at_r ( const intptr_t i_, const intptr_t j_, const intptr_t k_ ) const {
 
-	T at_r ( const intptr_t i_, const intptr_t j_, const intptr_t k_ ) const {
+        assert ( i_ >= BaseI && i_ < I + BaseI );
+        assert ( j_ >= BaseJ && j_ < J + BaseJ );
+        assert ( k_ >= BaseK && k_ < K + BaseK );
 
-		assert ( i_ >= BaseI && i_ < I + BaseI );
-		assert ( j_ >= BaseJ && j_ < J + BaseJ );
-		assert ( k_ >= BaseK && k_ < K + BaseK );
+        return ( m_data + I * J * K - 1 + BaseJ * K + BaseI * J * K + BaseK )[ K * ( -j_ - i_ * J ) - k_ ];
+    }
 
-		return ( m_data + I * J * K - 1 + BaseJ * K + BaseI * J * K + BaseK ) [ K * ( -j_ - i_ * J ) - k_ ];
-	}
+    T * data ( ) { return m_data; }
 
-	T *data ( ) {
+    const T * data ( ) const { return m_data; }
 
-		return m_data;
-	}
+    std::size_t size ( ) const { return I * J * K; }
 
-	const T *data ( ) const {
-
-		return m_data;
-	}
-
-	std::size_t size ( ) const {
-
-		return I * J * K;
-	}
-
-	extents_type extents ( ) const {
-
-		return extents_type { I, J, K };
-	}
+    extents_type extents ( ) const { return extents_type{ I, J, K }; }
 };
 
-
-template<typename T, intptr_t I, intptr_t J, intptr_t K, intptr_t L, intptr_t BaseI = 0, intptr_t BaseJ = 0, intptr_t BaseK = 0, intptr_t BaseL = 0, typename = std::enable_if_t < std::is_default_constructible<T>::value, T > >
+template<typename T, intptr_t I, intptr_t J, intptr_t K, intptr_t L, intptr_t BaseI = 0, intptr_t BaseJ = 0, intptr_t BaseK = 0,
+         intptr_t BaseL = 0, typename = std::enable_if_t<std::is_default_constructible<T>::value, T>>
 class HyperCube {
 
-	T m_data [ I * J * K * L ];
+    T m_data[ I * J * K * L ];
 
-public:
+    public:
+    HyperCube ( ) : m_data{ T ( ) } {}
+    HyperCube ( const HyperCube & h_ ) { memcpy ( m_data, h_.m_data, I * J * K * L * sizeof ( T ) ); }
+    HyperCube ( HyperCube && h_ ) { memcpy ( m_data, h_.m_data, I * J * K * L * sizeof ( T ) ); }
+    template<typename... Args>
+    HyperCube ( Args... a_ ) : m_data{ a_... } {}
 
-	HyperCube ( ) : m_data { T ( ) } { }
-	HyperCube ( const HyperCube & h_ ) { memcpy ( m_data, h_.m_data, I * J * K * L * sizeof ( T ) ); }
-	HyperCube ( HyperCube && h_ ) { memcpy ( m_data, h_.m_data, I * J * K * L * sizeof ( T ) ); }
-	template < typename ... Args >
-	HyperCube ( Args ... a_ ) : m_data { a_ ... } { }
+    typedef std::array<intptr_t, 4> extents_type;
 
-	typedef std::array < intptr_t, 4 > extents_type;
+    T & at ( const intptr_t i_, const intptr_t j_, const intptr_t k_, const intptr_t l_ ) {
 
-	T &at ( const intptr_t i_, const intptr_t j_, const intptr_t k_, const intptr_t l_ ) {
+        assert ( i_ >= BaseI && i_ < I + BaseI );
+        assert ( j_ >= BaseJ && j_ < J + BaseJ );
+        assert ( k_ >= BaseK && k_ < K + BaseK );
+        assert ( l_ >= BaseL && l_ < L + BaseL );
 
-		assert ( i_ >= BaseI && i_ < I + BaseI );
-		assert ( j_ >= BaseJ && j_ < J + BaseJ );
-		assert ( k_ >= BaseK && k_ < K + BaseK );
-		assert ( l_ >= BaseL && l_ < L + BaseL );
+        return ( m_data + L * ( K * ( -BaseJ - BaseI * J ) - BaseK ) - BaseL )[ L * ( K * ( j_ + i_ * J ) + k_ ) + l_ ];
+    }
 
-		return ( m_data + L * ( K * ( -BaseJ - BaseI * J ) - BaseK ) - BaseL ) [ L * ( K * ( j_ + i_ * J ) + k_ ) + l_ ];
-	}
+    T at ( const intptr_t i_, const intptr_t j_, const intptr_t k_, const intptr_t l_ ) const {
 
-	T at ( const intptr_t i_, const intptr_t j_, const intptr_t k_, const intptr_t l_ ) const {
+        assert ( i_ >= BaseI && i_ < I + BaseI );
+        assert ( j_ >= BaseJ && j_ < J + BaseJ );
+        assert ( k_ >= BaseK && k_ < K + BaseK );
+        assert ( l_ >= BaseL && l_ < L + BaseL );
 
-		assert ( i_ >= BaseI && i_ < I + BaseI );
-		assert ( j_ >= BaseJ && j_ < J + BaseJ );
-		assert ( k_ >= BaseK && k_ < K + BaseK );
-		assert ( l_ >= BaseL && l_ < L + BaseL );
+        return ( m_data + L * ( K * ( -BaseJ - BaseI * J ) - BaseK ) - BaseL )[ L * ( K * ( j_ + i_ * J ) + k_ ) + l_ ];
+    }
 
-		return ( m_data + L * ( K * ( -BaseJ - BaseI * J ) - BaseK ) - BaseL ) [ L * ( K * ( j_ + i_ * J ) + k_ ) + l_ ];
-	}
+    T * data ( ) { return m_data; }
 
-	T *data ( ) {
+    const T * data ( ) const { return m_data; }
 
-		return m_data;
-	}
+    std::size_t size ( ) const { return I * J * K * L; }
 
-	const T *data ( ) const {
-
-		return m_data;
-	}
-
-	std::size_t size ( ) const {
-
-		return I * J * K * L;
-	}
-
-	extents_type extents ( ) const {
-
-		return extents_type { I, J, K, L };
-	}
+    extents_type extents ( ) const { return extents_type{ I, J, K, L }; }
 };
 
-};
+}; // namespace ma
